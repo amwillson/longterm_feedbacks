318828ae697c25f4b107bb310eb2c54d65134294
Alyssa Willson <awillso2@nd.edu>
2022-12-09 18:45:46 -0500

    Converting utm to lat/long

--- FossilPollen/R/1.Format_UMW.R
+++ FossilPollen/R/1.Format_UMW.R
@@ -10,8 +10,9 @@ rm(list = ls())
 fc <- nc_open('FossilPollen/Data/msb-paleon-2/2Kyrs_Comp_Mean_Level2_v1.0.nc')
 
 # Get lon, lat, time
-x <- ncvar_get(fc, 'x')
-y <- ncvar_get(fc, 'y')
+# x and y are flipped to get the right easting and northing directions
+y <- ncvar_get(fc, 'x')
+x <- ncvar_get(fc, 'y')
 time <- ncvar_get(fc, 'Time')
 
 # Fractional composition
@@ -29,55 +30,55 @@ spruce <- ncvar_get(fc, 'SPRUCE')
 tamarack <- ncvar_get(fc, 'TAMARACK')
 
 # Replace dimension names
-dimnames(ash) <- list(x, y, time)
-dimnames(beech) <- list(x, y, time)
-dimnames(birch) <- list(x, y, time)
-dimnames(elm) <- list(x, y, time)
-dimnames(hemlock) <- list(x, y, time)
-dimnames(maple) <- list(x, y, time)
-dimnames(oak) <- list(x, y, time)
-dimnames(other_conifer) <- list(x, y, time)
-dimnames(other_hardwood) <- list(x, y, time)
-dimnames(pine) <- list(x, y, time)
-dimnames(spruce) <- list(x, y, time)
-dimnames(tamarack) <- list(x, y, time)
+dimnames(ash) <- list(y, x, time)
+dimnames(beech) <- list(y, x, time)
+dimnames(birch) <- list(y, x, time)
+dimnames(elm) <- list(y, x, time)
+dimnames(hemlock) <- list(y, x, time)
+dimnames(maple) <- list(y, x, time)
+dimnames(oak) <- list(y, x, time)
+dimnames(other_conifer) <- list(y, x, time)
+dimnames(other_hardwood) <- list(y, x, time)
+dimnames(pine) <- list(y, x, time)
+dimnames(spruce) <- list(y, x, time)
+dimnames(tamarack) <- list(y, x, time)
 
 # Reformat
 ash_melt <- melt(ash)
-colnames(ash_melt) <- c('x', 'y', 'time', 'ash')
+colnames(ash_melt) <- c('y', 'x', 'time', 'ash')
 
 beech_melt <- melt(beech)
-colnames(beech_melt) <- c('x', 'y', 'time', 'beech')
+colnames(beech_melt) <- c('y', 'x', 'time', 'beech')
 
 birch_melt <- melt(birch)
-colnames(birch_melt) <- c('x', 'y', 'time', 'birch')
+colnames(birch_melt) <- c('y', 'x', 'time', 'birch')
 
 elm_melt <- melt(elm)
-colnames(elm_melt) <- c('x', 'y', 'time', 'elm')
+colnames(elm_melt) <- c('y', 'x', 'time', 'elm')
 
 hemlock_melt <- melt(hemlock)
-colnames(hemlock_melt) <- c('x', 'y', 'time', 'hemlock')
+colnames(hemlock_melt) <- c('y', 'x', 'time', 'hemlock')
 
 maple_melt <- melt(maple)
-colnames(maple_melt) <- c('x', 'y', 'time', 'maple')
+colnames(maple_melt) <- c('y', 'x', 'time', 'maple')
 
 oak_melt <- melt(oak)
-colnames(oak_melt) <- c('x', 'y', 'time', 'oak')
+colnames(oak_melt) <- c('y', 'x', 'time', 'oak')
 
 other_conifer_melt <- melt(other_conifer)
-colnames(other_conifer_melt) <- c('x', 'y', 'time', 'other_conifer')
+colnames(other_conifer_melt) <- c('y', 'x', 'time', 'other_conifer')
 
 other_hardwood_melt <- melt(other_hardwood)
-colnames(other_hardwood_melt) <- c('x', 'y', 'time', 'other_hardwood')
+colnames(other_hardwood_melt) <- c('y', 'x', 'time', 'other_hardwood')
 
 pine_melt <- melt(pine)
-colnames(pine_melt) <- c('x', 'y', 'time', 'pine')
+colnames(pine_melt) <- c('y', 'x', 'time', 'pine')
 
 spruce_melt <- melt(spruce)
-colnames(spruce_melt) <- c('x', 'y', 'time', 'spruce')
+colnames(spruce_melt) <- c('y', 'x', 'time', 'spruce')
 
 tamarack_melt <- melt(tamarack)
-colnames(tamarack_melt) <- c('x', 'y', 'time', 'tamarack')
+colnames(tamarack_melt) <- c('y', 'x', 'time', 'tamarack')
 
 # Combine data frames
 full_melt <- ash_melt %>%
@@ -94,13 +95,47 @@ full_melt <- ash_melt %>%
   full_join(tamarack_melt, by = c('x', 'y', 'time'))
 
 # Combine x & y into one "location" variable
-full_melt <- full_melt %>%
-  mutate(loc = paste0(x,'_',y))
+#full_melt <- full_melt %>%
+#  mutate(loc = paste0(x,'_',y))
 
-# Add in time for unique time/x/y combinations
-full_melt <- full_melt %>%
-  mutate(loc_time = paste0(x,'_',y,'_',time))
+## Convert coordinates to lat, long
 
+# Make a dataframe of just the coordinates
+space <- full_melt %>%
+  dplyr::select(x,y)
+
+space <- as.data.frame(space)
+
+# Add coordinate info to x, y dataframe
+coordinates(space) <- ~y + x
+
+# Add projection. This is the projection according to the original netcdf file
+proj4string(space) <- CRS('+init=epsg:3175')
+
+# Reproject using wgs 84
+space_latlong <- spTransform(space, CRS('+init=epsg:4326'))
+# This gives the same result
+#space_latlong <- spTransform(space, CRS('+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0 '))
+
+# Add lat & long to full dataframe
+full_melt <- full_melt %>%
+  dplyr::select(-c(x,y)) %>%
+  mutate(long = space_latlong$y, lat = space_latlong$x)
+
+# Check to make sure the points match up with our expectation
+states <- map_data('state')
+states %>%
+  filter(region %in% c('illinois', 'indiana', 'michigan', 'minnesota', 'wisconsin')) %>%
+  ggplot() +
+  geom_polygon(aes(x = long, y = lat, group = group), fill = 'white', color = 'black') +
+  coord_map('albers', lat0 = 45.5, lat1 = 29.5) +
+  geom_point(data = full_melt, aes(x = long, y = lat, color = hemlock), alpha = 0.05)
+
+# Add in time for unique time/lat/long combinations
+full_melt <- full_melt %>%
+  mutate(loc_time = paste0(lat,'_',long,'_',time))
+
+# Remove empty grid cells
 for_removal <- c()
 
 for(i in 1:nrow(full_melt)){
@@ -114,5 +149,13 @@ for(i in 1:nrow(full_melt)){
 
 full_melt_rm <- full_melt[-for_removal,]
 full_melt <- full_melt_rm
+
+# Check the final extent of the data
+states %>%
+  filter(region %in% c('illinois', 'indiana', 'michigan', 'minnesota', 'wisconsin')) %>%
+  ggplot() +
+  geom_polygon(aes(x = long, y = lat, group = group), fill = 'white', color = 'black') +
+  coord_map('albers', lat0 = 45.5, lat1 = 29.5) +
+  geom_point(data = full_melt, aes(x = long, y = lat, color = hemlock), alpha = 0.05)
 
 save(full_melt, file = 'FossilPollen/Data/full_melt_UMW.RData')
